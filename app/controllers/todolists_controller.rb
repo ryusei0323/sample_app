7a7658838aa6169c30c0453cf45213dc30e6e8de
vagrant <vagrant@localhost.localdomain>
2020-07-09 12:17:56 +0000

    finish rails-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -8,9 +8,18 @@ class TodolistsController < ApplicationController
     list =List.new(list_params)
     # ２. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # ３. トップ画面へリダイレクト
-    redirect_to '/top'
+    # ３. 詳細画面へリダイレクト
+    redirect_to todolist_path(list.id)
   end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
   private
   # ストロングパラメータ
   def list_params
