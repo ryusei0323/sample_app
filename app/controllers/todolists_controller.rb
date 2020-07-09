1440ad92894846537941dedb905328f2e885b199
vagrant <vagrant@localhost.localdomain>
2020-07-09 15:36:24 +0000

    finish rails-7

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -2,7 +2,6 @@ class TodolistsController < ApplicationController
   def new
   	@list = List.new
   end
-
   def create
     # １. データを新規登録するためのインスタンス作成
     list =List.new(list_params)
@@ -11,13 +10,19 @@ class TodolistsController < ApplicationController
     # ３. 詳細画面へリダイレクト
     redirect_to todolist_path(list.id)
   end
-
   def index
     @lists = List.all
   end
-
   def show
     @list = List.find(params[:id])
+  end
+  def edit
+    @list = List.find(params[:id])
+  end
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
   end
 
   private
