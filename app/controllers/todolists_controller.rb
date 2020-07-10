7aa5622576821234e25c6ac0e206253f20518396
vagrant <vagrant@localhost.localdomain>
2020-07-10 16:00:42 +0000

    finish rails-9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -3,11 +3,8 @@ class TodolistsController < ApplicationController
   	@list = List.new
   end
   def create
-    # １. データを新規登録するためのインスタンス作成
-    list =List.new(list_params)
-    # ２. データをデータベースに保存するためのsaveメソッド実行
+    list = List.new(list_params)
     list.save
-    # ３. 詳細画面へリダイレクト
     redirect_to todolist_path(list.id)
   end
   def index
@@ -23,6 +20,11 @@ class TodolistsController < ApplicationController
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
+  end
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to todolists_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
   get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
   patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
-  # .../todolists/1 や .../todolists/3 に該当する
+  delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
 end
